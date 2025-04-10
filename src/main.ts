4219fe62f5a860931c38ee3ebe349555e43a545b
MouhibBahri <mouhib.bahri@insat.ucar.tn>
2025-04-10 19:05:08 +0100

    Enable swagger, configure pipes  and add typeorm

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Tp Rest API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Automatically transform payloads
+      whitelist: true, // Strip non-whitelisted properties
+      forbidNonWhitelisted: true, // Throw errors for non-whitelisted properties
+    }),
+  );
+  app.useGlobalFilters();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
